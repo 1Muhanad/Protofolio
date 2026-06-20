3e95d4d02db7e22bd6011408f422d4bc6aebaeb0
1Muhanad <muhanadelaidy@gmail.com>
2026-06-20 19:57:36 +0300

    Update Informations

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/foooter/Footer";
 import Home from "./components/home/Home";
-import Nav from "./components/nav/Nav";
+import Nav from "./components/nav/Nav.jsx";
 import Projects from "./components/projects/Projects";
 import Services from "./components/services/Services";
 import Skills from "./components/skills/Skills";
